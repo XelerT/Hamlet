d6e051e66f09268717e9326dc9fb08725c3a25b1
Alex Taranov <taranov.sasha2004@gmail.com>
2022-09-16 15:07:14 +0300

    [4] Added Hamlet, fixed bugs, added calloc check.
    - Added Hamlet text.
    - Fixed bug with calloc, didn't work with big texts.
    - Added check for NULL return of callocs.

--- main.cpp
+++ main.cpp
@@ -6,11 +6,14 @@
 #include <math.h>
 
 char **get_text (FILE *text);
-char **devide_text (char *buf, int n_lines);
+char **divide_text (char *buf, int n_lines);
 void free_ptrs (char **lines);
 int my_strcmp (const char *lhs, const char *rhs);
 void print_buf (const char **lines);
 void bubble_sort_lines (char **lines);
+int err_print (int num);
+char** check_pp_calloc (char **ptr);
+char* check_p_calloc (char *ptr);
 
 int main ()
 {
@@ -21,10 +24,10 @@ int main ()
         output = fopen("output.txt", "w");
 
         if (text == nullptr)
-                return 1;
+                return err_print(1);
 
         else if (output == nullptr)
-                return 2;
+                return err_print(2);
 
         char **lines = get_text(text);
 
@@ -42,34 +45,40 @@ int main ()
 
 char **get_text (FILE *text)
 {
+        // printf("get_text1\n");
         struct stat file;
         stat("input.txt", &file);
         int n_chars = 0;
-
-        char *buf = (char*) calloc(sizeof(file.st_size) + 1, sizeof(char));
+        // printf("get_text2 file size: %d\n", sizeof(file.st_size));
+        char *buf = check_p_calloc((char*) calloc(file.st_size + 1, sizeof(char)));
+        assert(buf);
         n_chars = fread(buf, sizeof(char), file.st_size, text);
+        // printf("get_text4 n_chars %d\n", n_chars);
 
         buf[n_chars] = '\n';
-        int n_lines = (file.st_size / sizeof(char)) - n_chars;
+        int n_lines = file.st_size - n_chars;
+        n_lines++;
+        // printf("get_text5 n_lines %d\n", n_lines);
 
-        return devide_text(buf, n_lines);
+        return divide_text(buf, n_lines);
 }
 
-char **devide_text (char *buf, int n_lines)
+char **divide_text (char *buf, int n_lines)
 {
         assert(buf);
-
-        char **lines = (char**) calloc(n_lines + 1, sizeof(char*));
+        // printf("QWERTY\n");
+        char **lines = check_pp_calloc((char**) calloc(n_lines + 1, sizeof(char*)));
+        //printf("Divide_text2\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\n");
         lines[n_lines] = nullptr;
-// printf("/%d\n", n_lines);
-        for (int i = 0; i < n_lines; i++)
-        {
+//  printf("/%d\n", n_lines);
+        for (size_t i = 0; i < n_lines; i++) {
                 lines[i] = buf;
-                // printf("%s\n", buf);
                 while (*buf != '\n' && *buf != '\0')
                         buf++;
                 buf++;
+                //printf("Divide_text3 %d %d\n", i, n_lines);
         }
+        // printf("Divide_text4\n");
 // printf("\\\n");
         assert(*lines);
 
@@ -111,8 +120,7 @@ void bubble_sort_lines (char **lines)
 
         for (int i = 0; lines[i] != nullptr; i++)
                 for (int j = i + 1; lines[j] != nullptr; j++)
-                        if (my_strcmp(lines[i], lines[j]) > 0)
-                        {
+                        if (my_strcmp(lines[i], lines[j]) > 0) {
                                 swap = lines[i];
                                 lines[i] = lines[j];
                                 lines[j] = swap;
@@ -126,8 +134,7 @@ void print_buf (const char **lines)
 
         // printf("QWERTY\n%s\n\n", lines[0]);
 
-        while (*lines != nullptr)
-        {
+        while (*lines != nullptr) {
                 for (int i = 0; (*lines)[i] != '\n'; i++)
                         putchar((*lines)[i]);
                         // printf("%d\n", i);
@@ -138,3 +145,34 @@ void print_buf (const char **lines)
         // printf("%s\n", *(--lines));
 }
 
+int err_print (int num)
+{
+        switch (num) {
+        case 1:
+                printf("Input file pointer is empty.\n");
+                break;
+        case 2:
+                printf("Output file pointer is empty.\n");
+                break;
+        }
+
+        return num;
+}
+
+char** check_pp_calloc (char **ptr)
+{
+        if (!ptr) {
+                printf("Calloc returned NULL.\n");
+        }
+
+        return ptr;
+}
+
+char* check_p_calloc (char *ptr)
+{
+        if (!ptr) {
+                printf("Calloc returned NULL.\n");
+        }
+
+        return ptr;
+}
