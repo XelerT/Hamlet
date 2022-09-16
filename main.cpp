a54c98b53c561873275f12cd401b6eb75e52151b
Alex Taranov <taranov.sasha2004@gmail.com>
2022-09-16 20:46:28 +0300

    [5] Added quick sort, swap function.

--- main.cpp
+++ main.cpp
@@ -14,10 +14,12 @@ void bubble_sort_lines (char **lines);
 int err_print (int num);
 char** check_pp_calloc (char **ptr);
 char* check_p_calloc (char *ptr);
+void swap_lines (char **lines, int i, int j);
+void q_sort_lines (char **lines, int left, int right);
 
 int main ()
 {
-        FILE *text  = nullptr;
+        FILE *text   = nullptr;
         FILE *output = nullptr;
 
         text   = fopen("input.txt",  "r");
@@ -30,11 +32,7 @@ int main ()
                 return err_print(2);
 
         char **lines = get_text(text);
-
-        //         printf("1\n");
-        // print_buf((const char**) lines);
-        bubble_sort_lines(lines);
-                printf("2\n");
+        //q_sort_lines(lines, 0, );
         print_buf((const char**) lines);
 
         free_ptrs(lines);
@@ -45,20 +43,16 @@ int main ()
 
 char **get_text (FILE *text)
 {
-        // printf("get_text1\n");
         struct stat file;
         stat("input.txt", &file);
         int n_chars = 0;
-        // printf("get_text2 file size: %d\n", sizeof(file.st_size));
         char *buf = check_p_calloc((char*) calloc(file.st_size + 1, sizeof(char)));
         assert(buf);
         n_chars = fread(buf, sizeof(char), file.st_size, text);
-        // printf("get_text4 n_chars %d\n", n_chars);
 
         buf[n_chars] = '\n';
         int n_lines = file.st_size - n_chars;
         n_lines++;
-        // printf("get_text5 n_lines %d\n", n_lines);
 
         return divide_text(buf, n_lines);
 }
@@ -66,20 +60,15 @@ char **get_text (FILE *text)
 char **divide_text (char *buf, int n_lines)
 {
         assert(buf);
-        // printf("QWERTY\n");
         char **lines = check_pp_calloc((char**) calloc(n_lines + 1, sizeof(char*)));
-        //printf("Divide_text2\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\n");
         lines[n_lines] = nullptr;
-//  printf("/%d\n", n_lines);
         for (size_t i = 0; i < n_lines; i++) {
                 lines[i] = buf;
                 while (*buf != '\n' && *buf != '\0')
                         buf++;
                 buf++;
-                //printf("Divide_text3 %d %d\n", i, n_lines);
         }
-        // printf("Divide_text4\n");
-// printf("\\\n");
+
         assert(*lines);
 
         return lines;
@@ -116,15 +105,35 @@ void bubble_sort_lines (char **lines)
         assert(lines);
         assert(*lines);
 
-        char *swap = nullptr;
-
         for (int i = 0; lines[i] != nullptr; i++)
                 for (int j = i + 1; lines[j] != nullptr; j++)
-                        if (my_strcmp(lines[i], lines[j]) > 0) {
-                                swap = lines[i];
-                                lines[i] = lines[j];
-                                lines[j] = swap;
-                        }
+                        if (my_strcmp(lines[i], lines[j]) > 0)
+                                swap_lines(lines, i, j);
+}
+
+void q_sort_lines (char **lines, int left, int right)
+{
+        int i = 0;
+        int last = 0;
+
+        if (left >= right)
+                return;
+        swap_lines(lines, left, (left + right) / 2);
+        last = left;
+        for (i = left + 1; i <= right; i++)
+                if (my_strcmp(lines[i], lines[left]) > 0)
+                        swap_lines(lines, i, ++last);
+        swap_lines(lines, left, last);
+        q_sort_lines(lines, left, last - 1);
+        q_sort_lines(lines, last + 1, right);
+}
+
+void swap_lines (char **lines, int i, int j)
+{
+        char *swap = nullptr;
+        swap = lines[i];
+        lines[i] = lines[j];
+        lines[j] = swap;
 }
 
 void print_buf (const char **lines)
@@ -132,17 +141,12 @@ void print_buf (const char **lines)
         assert(lines);
         assert(*lines);
 
-        // printf("QWERTY\n%s\n\n", lines[0]);
-
         while (*lines != nullptr) {
                 for (int i = 0; (*lines)[i] != '\n'; i++)
                         putchar((*lines)[i]);
-                        // printf("%d\n", i);
-                // printf(" %s\n", *lines);
                 putchar('\n');
                 *(lines++);
         }
-        // printf("%s\n", *(--lines));
 }
 
 int err_print (int num)
